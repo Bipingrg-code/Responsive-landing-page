cbf2913f0136bb9861790236ccb109299917a31f
bipin67678grg@gmail.com <bipin67678grg@gmail.com>
2023-04-18 10:54:07 +0545

    fix img url

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // Responsive
-$(function() {
-	menu = $('nav ul');
+$(function () {
+  menu = $('nav ul');
 
-  $('#openup').on('click', function(e) {
+  $('#openup').on('click', function (e) {
     e.preventDefault(); menu.slideToggle();
   });
-  
-  $(window).resize(function(){
-    var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
+
+  $(window).resize(function () {
+    var w = $(this).width(); if (w > 480 && menu.is(':hidden')) {
       menu.removeAttr('style');
     }
   });
-  
-  $('nav li').on('click', function(e) {                
-    var w = $(window).width(); if(w < 480 ) {
-      menu.slideToggle(); 
+
+  $('nav li').on('click', function (e) {
+    var w = $(window).width(); if (w < 480) {
+      menu.slideToggle();
     }
   });
   $('.open-menu').height($(window).height());
@@ -22,20 +22,21 @@ $(function() {
 
 //Smothscrolling
 
-$('.cf a').on('click',function(event) {
-    if(this.hash !== ''){
-        event.preventDefault();
+// $('.cf a').on('click',function(event) {
+//     if(this.hash !== ''){
+//         event.preventDefault();
 
-        const hash = this.hash;
+//         const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top
-            },
-            800,
-            function () {
-                window.location.hash = hash;
-            }
-        );
-    }
-});
+//         $('html, body').animate(
+//             {
+//                 scrollTop: $(hash).offset().top
+//             },
+//             800,
+//             function () {
+//                 window.location.hash = hash;
+//             }
+//         );
+//     }
+// });
+
